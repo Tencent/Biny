25c16bfba301f80c17f7102dc1190fcadfddbc14
billge1205 <billge_1205@sina.com>
2019-12-23 10:47:25 +0800

    add 网站备案号

--- app/template/base/footer.tpl.php
+++ app/template/base/footer.tpl.php
@@ -1,3 +1,9 @@
+<div style="width: 100%;height: 25px; background: black; ">
+    <div class="container">
+        <a href="https://github.com/Tencent/Biny" style="color: white;text-decoration:none" target="_blank">Biny官网</a>
+        <a href="http://www.beian.miit.gov.cn" style="color: white; margin-left: 10px;text-decoration:none" target="_blank">沪ICP备17003400号-1</a>
+    </div>
+</div>
 </body>
 </html>
 
