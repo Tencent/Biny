7b540b33a37060c99c96d1f9ef14a6dc52565bbe
billge1205 <billge_1205@sina.com>
2020-03-23 14:00:27 +0800

    网站备案整改

--- app/template/base/footer.tpl.php
+++ app/template/base/footer.tpl.php
@@ -1,6 +1,6 @@
-<div style="width: 100%;height: 25px; background: black; ">
+<div style="width: 100%;height: 30px; line-height: 30px; background: black; ">
     <div class="container">
-        <a href="https://github.com/Tencent/Biny" style="color: white;text-decoration:none" target="_blank">Biny官网</a>
+        <a href="https://github.com/Tencent/Biny" style="color: white;text-decoration:none" target="_blank">Biny框架文档</a>
         <a href="http://www.beian.miit.gov.cn" style="color: white; margin-left: 10px;text-decoration:none" target="_blank">沪ICP备17003400号-1</a>
     </div>
 </div>
